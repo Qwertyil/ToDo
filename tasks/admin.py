ab930d63e00516cd3341c13bfca3987996407477
Amir <zamnatov2004@gmail.com>
2025-05-03 03:32:24 +0300

    Tasks model created and added to admin page

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Task
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'is_completed', 'creation_time', 'completion_time')
+    list_filter = ('is_completed', 'creation_time')
+    search_fields = ('title', 'additional')
+    fields = ('title', 'additional', 'creation_time', 'is_completed', 'completion_time')
+    readonly_fields = ('creation_time', 'is_completed', 'completion_time')
+
+
+admin.site.register(Task, TaskAdmin)
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Задача')
+    additional = models.TextField(verbose_name='Дополнительная информация', null=True, blank=True)
+    creation_time = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    is_completed = models.BooleanField(default=False, verbose_name='Выполнена')
+    completion_time = models.DateTimeField(null=True, blank=True, verbose_name='Дата выполнения')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+        ordering = ['is_completed', '-creation_time']
