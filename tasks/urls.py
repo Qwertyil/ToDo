6d24ef0f3f5c84a2f6109523d294ff84c354a787
Amir <zamnatov2004@gmail.com>
2025-05-04 04:35:01 +0300

    Created main page to see uncompleted tasks, mark them as completed and created new tasks

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 app_name = 'tasks'
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name='index'),
+    path('new_task/', views.TaskCreateView.as_view(), name='new_task'),
+    path('<int:pk>/', views.mark_task_as_completed, name='mark_task_as_completed')
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,46 @@
-from django.http import HttpResponse
+from django.http import JsonResponse
+from django.urls import reverse_lazy
+from django.views.decorators.http import require_http_methods
+from django.views.generic.edit import CreateView
+from django.shortcuts import redirect
 from django.shortcuts import render
+
+from .models import Task
+from .forms import TaskForm
 
 
 def index(request):
-    return HttpResponse('Its working')
+    return render(request, 'tasks/index.html', context={'tasks': Task.objects.filter(is_completed=False)})
+
+
+@require_http_methods(['PATCH'])
+def mark_task_as_completed(request, pk):
+    try:
+        task = Task.objects.get(pk=pk)
+        task.is_completed = True
+        task.save()
+        # Явно возвращаем успешный ответ
+
+        return JsonResponse({
+            'status': 'success',
+            'message': 'Задача успешно обновлена'
+        }, status=200)
+
+    except Task.DoesNotExist:
+        return JsonResponse({
+            'status': 'error',
+            'message': 'Задача не найдена'
+        }, status=404)
+
+    except Exception as e:
+        return JsonResponse({
+            'status': 'error',
+            'message': str(e)
+        }, status=500)
+
+
+class TaskCreateView(CreateView):
+    template_name = 'tasks/new_task.html'
+    form_class = TaskForm
+    success_url = reverse_lazy('tasks:index')
+
